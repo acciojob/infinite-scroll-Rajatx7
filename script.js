8fb83adcc041eb603613c585e44f292e8e2bfc64
Rajat Singh <155432801+Rajatx7@users.noreply.github.com>
2024-02-05 22:38:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //your code here!
 
+const dynamicList = document.getElementById('infi-list');
+  let itemCount = 10;
+
+ function addMoreItems() {
+    // Add 2 more items to the list
+    for (let i = 0; i < 2; i++) {
+      itemCount++;
+      const newItem = document.createElement('li');
+      newItem.textContent = `Item ${itemCount}`;
+      dynamicList.appendChild(newItem);
+    }
+  }
+
+ dynamicList.addEventListener('scroll', function() {
+    if (dynamicList.scrollTop + dynamicList.clientHeight >= dynamicList.scrollHeight) {
+      addMoreItems();
+    }
+  });
